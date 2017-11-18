8b1cde51b7adbf224b06b7f3cde3bcb7a472710e
Jett <whackashoe@gmail.com>
2017-11-17 20:06:49 -0600

    move scripts to its own dir

--- export_all_maps.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-find tp_maps/ -name "*.json" | while read file;
-do
-    base=`basename ${file} .json`
-    ./tagos export "tp_maps/${base}.json" "tp_maps/${base}.png" "maps/${base}.json"
-done
