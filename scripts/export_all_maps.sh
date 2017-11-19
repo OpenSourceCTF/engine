19220f326d659de07e9b2ab5388dac72e7fb61b9
Jett <whackashoe@gmail.com>
2017-11-18 20:21:50 -0600

    save failed exports to log

--- scripts/export_all_maps.sh
+++ scripts/export_all_maps.sh
@@ -3,5 +3,5 @@
 find ./tp_maps/ -name "*.json" | while read file;
 do
     base=`basename ${file} .json`
-    ./tagos export "./tp_maps/${base}.json" "./tp_maps/${base}.png" "./maps/${base}.json"
+    ./tagos export "./tp_maps/${base}.json" "./tp_maps/${base}.png" "./maps/${base}.json" || echo ${base} >> export_all_maps.failed.log
 done
